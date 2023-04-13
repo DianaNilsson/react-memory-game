e5462db8f546216abd0532f075f9e9a7f4c97a84
Diana <diana.sofie.nilsson@gmail.com>
2023-04-13 11:06:21 +0200

    Render Board component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css'
+import Board from './components/board/Board'
 
 const App = () => {
   return (
     <div className="App">
       <h1>Kids Memory</h1>
       <h2>Starta spelet genom att klicka på ett av korten</h2>
+      <Board />
     </div>
   )
 }
