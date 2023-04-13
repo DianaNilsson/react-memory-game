c7b639bbae1a6d601a72fa30d8b873bd27857f57
Diana <diana.sofie.nilsson@gmail.com>
2023-04-13 11:06:00 +0200

    Correct the image src

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -1,16 +1,20 @@
 import React, { useState } from 'react'
-import Card from './Card'
+import Card from '../card/Card'
+import cardBack1 from '../../assets/images/card-back-1.jpg'
+import cardBack2 from '../../assets/images/card-back-2.jpg'
+import cardBack3 from '../../assets/images/card-back-3.jpg'
+import cardBack4 from '../../assets/images/card-back-4.jpg'
 
 const Board = () => {
   const [cards, setCards] = useState([
-    { id: 1, image: 'assets/images/card-back-1.jpg' },
-    { id: 2, image: 'assets/images/card-back-1.jpg' },
-    { id: 3, image: 'assets/images/card-back-2.jpg' },
-    { id: 4, image: 'assets/images/card-back-2.jpg' },
-    { id: 5, image: 'assets/images/card-back-3.jpg' },
-    { id: 6, image: 'assets/images/card-back-3.jpg' },
-    { id: 7, image: 'assets/images/card-back-4.jpg' },
-    { id: 8, image: 'assets/images/card-back-4.jpg' }
+    { id: 1, image: cardBack1 },
+    { id: 2, image: cardBack1 },
+    { id: 3, image: cardBack2 },
+    { id: 4, image: cardBack2 },
+    { id: 5, image: cardBack3 },
+    { id: 6, image: cardBack3 },
+    { id: 7, image: cardBack4 },
+    { id: 8, image: cardBack4 }
   ])
 
   return (
